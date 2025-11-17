e280d7e03e968f1da7018473aed95bb21e6db481
tahsin090 <tajisan789@gmail.com>
2025-11-17 20:46:05 +0600

    solved problem1

--- solution.ts
+++ solution.ts
@@ -1,1 +1,18 @@
-const dd = 23121
+const formatValue = (value: string | number | boolean) => {
+    if (typeof value === "string") {
+        return value.toUpperCase()
+    }
+    else if (typeof value === "number") {
+        return value * value
+    }
+    else if (typeof value === "boolean") {
+        return !value
+    }
+    else {
+        return value
+    }
+}
+
+console.log(formatValue('hello'));
+console.log(formatValue(5));
+console.log(formatValue(true));
